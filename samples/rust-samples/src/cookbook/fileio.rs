c18091630e9103a3dea6fc1c7cebd4910b7fea58
Adam Lock <locka99@gmail.com>
2016-11-23 23:09:53 +0000

    Add a mkdir test

--- samples/rust-samples/src/cookbook/fileio.rs
+++ samples/rust-samples/src/cookbook/fileio.rs
@@ -1,6 +1,25 @@
 // use std::fs::prelude::*;
 
+use std::path::{Path, PathBuf};
+use std::env::{temp_dir};
+
 #[test]
 fn mkdir_recursive() {
-    // let result = DirBuilder::new().recursive(true).create("/tmp/work_dir");
+    //
+    use std::fs::{self, DirBuilder};
+    use std::env::{temp_dir};
+    let mut path: PathBuf = temp_dir();
+    path.push(Path::new("test_dir"));
+    let result = DirBuilder::new().recursive(true).create(path.as_path());
+    assert!(fs::metadata(path).unwrap().is_dir());
+}
+
+#[test]
+fn create_file() {
+
+}
+
+#[test]
+fn open_file() {
+
 }
