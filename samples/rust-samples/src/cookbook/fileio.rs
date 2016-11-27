ce9672ea76d8057332dc3561cdc0c9db4df456d3
Adam Lock <locka99@gmail.com>
2016-11-27 20:11:59 +0000

    Add some file test

--- samples/rust-samples/src/cookbook/fileio.rs
+++ samples/rust-samples/src/cookbook/fileio.rs
@@ -3,6 +3,12 @@
 use std::path::{Path, PathBuf};
 use std::env::{temp_dir};
 
+fn make_file_path(filename: &str) -> String {
+    let mut path: PathBuf = temp_dir();
+    path.push(filename);
+    path.as_path().to_str().unwrap().to_string()
+}
+
 #[test]
 fn mkdir_recursive() {
     //
@@ -16,10 +22,24 @@ fn mkdir_recursive() {
 
 #[test]
 fn create_file() {
+    use std::io::prelude::*;
+    use std::fs::File;
 
+    let mut create_result = File::create(make_file_path("created.txt"));
+    assert!(create_result.is_ok());
+    let mut f = create_result.unwrap();
+    for i in 0..100 {
+        write!(f, "Line {}\n", i);
+    }
 }
 
 #[test]
 fn open_file() {
+    use std::io::prelude::*;
+    use std::fs::File;
+
+    let mut open_result = File::open(make_file_path("open.txt"));
+    assert!(open_result.is_ok());
+    let mut f = open_result.unwrap();
 
 }
